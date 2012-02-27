aa2db148a3cd23eaddd3d3f65d1aa4f23eaf1c6f
Martin Riesz <riesz.martin@gmail.com>
2012-02-27 20:04:47 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+ruby-buxus-client
+=================
+
+It is a [Buxus CMS](http://www.ui42.sk/cms-buxus.html) client library written in Ruby.
+
+*configuration:* change `BASE_URL` to point to your Buxus URL and name your SSL certificate file as `cert.pem`.
+
+*Example usage:*
+
+    buxus = Buxus.new.login("yourusername", "yourpassword")
+    buxus.create_page(page_id)
+    buxus.update_page(page_id, page_title, html)
+    buxus.set_page_active(page_id, true) # makes page public
+    buxus.clear_archive(page_id, 10)     # purge old page history, maintains 10 latest versions
+    buxus.delete_page(current_page_id)   # deletes page
+
+Tested with Buxus 5.5.7, Mechanize 0.9.3, Ruby 1.8
+
+Code license: [GPLv3](http://www.gnu.org/licenses/gpl.html)
---
+++ buxus.rb
@@ -0,0 +1,97 @@
+require 'mechanize'
+
+class Buxus
+
+  BASE_URL = "https://www.example.com"
+  
+  def initialize
+    @agent = WWW::Mechanize.new do |agent|
+      agent.ca_file = File.join(File.dirname(__FILE__), "cert.pem")
+      agent.follow_meta_refresh = true
+      agent.keep_alive = false
+    end
+  end
+
+  def login(login, password)
+    @agent.get(BASE_URL + "/system/login.php") do |page|
+      page.form_with(:name => "login_form") do |form|
+	form.login = login
+	form.password = password
+      end.submit
+    end
+    self
+  end
+  
+  def update_page(page_id, title, html)
+    @agent.get(BASE_URL + "/system/page_details.php?page_id=#{page_id}") do |page|
+      page.form_with(:name => "page_details") do |form|
+	form["property[1]"]  = "Generovane_#{title}" # "Nazov"
+	form["property[12]"] = title # "Title"
+	form["property[13]"] = title # "Skrateny nazov" (menu)
+	second_textarea_name = page.search("/html/body/table[@id='layouttable']/tr[2]/td[@id='mainbodytd']/table/tr/td/form[@id='page_details']/table[2]/tr[td[1]/div/div[1]='Text:']/td/textarea")[0]["name"]
+	form[second_textarea_name] = html   # "Text"
+      end.submit
+    end
+    self
+  end
+
+  def clear_archive(page_id, num_to_keep = 0)
+    @agent.get(BASE_URL + "/lib/archive/uif/page_history.php?page_id=#{page_id}") do |page|
+      page.form_with(:name => "page_history") do |form|
+	form.checkboxes_with(:name => "selected_items[]")[num_to_keep..-1].each do |checkbox|
+	  checkbox.check
+	end rescue nil #nil.each
+      end.submit
+    end
+    self
+  rescue URI::InvalidURIError
+    self
+  end
+
+  def get_content_types(parent_page_id)
+    page = @agent.get(BASE_URL + "/system/page_details.php?page_id=#{parent_page_id}")
+    content_types = []
+    page.search("/html/body/table[@id='layouttable']/tr[2]/td[1]/table[@id='leftmenutable']/tr/td/table[@id='mainmenutable']/tr[2]/td/table[@id='contextmenutable']/tr/td[@class='yellow-menu']/a").each do |a|
+      if a.content =~ /^Vložiť /
+	content_types << a["href"].sub(/^page_details.php.parent_page_id=\d+&show_type=insert&page_type_id=(\d+).*$/, '\1').to_i
+      end
+    end
+    content_types
+  end
+
+  def create_page(parent_page_id, content_type = get_content_types(parent_page_id).min)
+    page = @agent.get(BASE_URL + "/system/page_details.php?parent_page_id=#{parent_page_id}&show_type=insert&page_type_id=#{content_type}")
+    page_details = page.form_with(:name => "page_details") do |form|
+      form["property[1]"] = "empty"
+    end.submit
+    page_id = page_details.search("/html/body/table[@id='layouttable']/tr[2]/td[@id='mainbodytd']/table/tr/td/form[@id='page_details']/table[1]/tr[td[1]/span='ID:']/td[2]")[0].content.to_i
+  end
+
+  def delete_page(page_id)
+    @agent.get(BASE_URL + "/system/delete_confirmation.php?page_id=#{page_id}") do |page|
+      page.form_with(:name => "delete_confirmation") do |form| 
+      end.click_button
+    end
+    self
+  rescue WWW::Mechanize::ResponseCodeError
+    self
+  end
+
+  def set_page_active(page_id, active = true)
+    @agent.get(BASE_URL + "/system/page_details.php?page_id=#{page_id}") do |page|
+      page.form_with(:name => "page_details") do |form|
+	if active
+	  form["property[9]"] = 1
+	else
+	  form["property[9]"] = 2
+	end
+      end.submit
+    end
+    self
+  end
+  
+  def agent
+    @agent
+  end
+  
+end
